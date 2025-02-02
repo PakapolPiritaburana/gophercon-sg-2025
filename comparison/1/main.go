51972f20632b4799e54e319d39d708ee22886a0a
Pakapol <pakapol_p@muangthai.co.th>
2025-02-02 19:31:26 +0700

    add fail compare for example

--- comparison/1/main.go
+++ comparison/1/main.go
@@ -6,24 +6,18 @@ import (
 )
 
 func main() {
-	//sliceA := []string{"a", "b", "c"}
 	sliceA := []int{1, 2}
-	//sliceA := make([]string, 3)
-	//sliceA[0] = "a"
-	//sliceA[1] = "b"
-	//sliceA[2] = "c"
 	sliceB := []int{1, 2}
-	//sliceB := []string{"a", "b", "c"}
 
-	if sliceA[0] == sliceB[0] {
-		fmt.Println("Index 0 = Equal")
+	if sliceA == sliceB {
+		fmt.Println("== Equal")
 	} else {
-		fmt.Println("Index 0 = Not equal")
+		fmt.Println("== Not equal")
 	}
 
 	if reflect.DeepEqual(sliceA, sliceB) {
-		fmt.Println("DeepEqual = Equal")
+		fmt.Println("DeepEqual Equal")
 	} else {
-		fmt.Println(" DeepEqual = Not equal")
+		fmt.Println("DeepEqual Not equal")
 	}
 }
