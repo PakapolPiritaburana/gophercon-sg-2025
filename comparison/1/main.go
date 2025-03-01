a47b33ce2a5a4e82250cafc8f5a1c197c3d3839d
Pakapol <pakapol_p@muangthai.co.th>
2025-03-01 15:39:08 +0700

    comment invalid code

--- comparison/1/main.go
+++ comparison/1/main.go
@@ -9,11 +9,11 @@ func main() {
 	sliceA := []int{1, 2}
 	sliceB := []int{1, 2}
 
-	if sliceA == sliceB {
-		fmt.Println("== Equal")
-	} else {
-		fmt.Println("== Not equal")
-	}
+	//if sliceA == sliceB {
+	//	fmt.Println("== Equal")
+	//} else {
+	//	fmt.Println("== Not equal")
+	//}
 
 	if reflect.DeepEqual(sliceA, sliceB) {
 		fmt.Println("DeepEqual Equal")
